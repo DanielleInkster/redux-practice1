21a2df9ccc1711b8ae04f7fc3f9a3de77ebb1437
DanielleInkster <danielle.inkster@gmail.com>
2020-06-05 16:15:36 -0600

    Increments by 1

--- reduxtest.js
+++ reduxtest.js
@@ -5,6 +5,17 @@ const initialSate = {
 }
 
 const myReducer =(state = initialSate, action)=>{
+    const newState = {...state}
+
+    if (action.type === 'ADD'){
+        newState.age += 1
+    }
+
+    return newState
 }
 
 const store = createStore(myReducer);
+
+store.dispatch({type: 'ADD'});
+
+console.log(store.getState())
